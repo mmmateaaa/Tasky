180b5b23cf7ce9eac2d5684489e0d9fecdc91abb
Matea <sim.mat06@gmail.com>
2018-04-17 19:08:11 +0200

    Added Butterknife, Picasso and RecyclerView

---
+++ app/src/main/java/hr/ferit/matea/tasky/List.java
@@ -0,0 +1,13 @@
+package hr.ferit.matea.tasky;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
